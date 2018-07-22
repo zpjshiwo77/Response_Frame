446a272e064c7b6db1ac49ebe3ba73fd25d43069
page <page@be-biu.com>
2018-07-22 12:03:25 +0800

    新增转换时间格式的方法

--- js/public/common.js
+++ js/public/common.js
@@ -194,6 +194,23 @@ function importCom() {
 			if(onError) onError();
 		}//end else
 	}//edn func
+
+	//时间格式转换
+	_self.formatDateTime = function(date,detail,sign){
+	    var y = date.getFullYear();    
+	    var m = date.getMonth() + 1;    
+	    m = m < 10 ? ('0' + m) : m;    
+	    var d = date.getDate();    
+	    d = d < 10 ? ('0' + d) : d;    
+	    var h = date.getHours();  
+	    h = h < 10 ? ('0' + h) : h;  
+	    var minute = date.getMinutes();  
+	    var second = date.getSeconds();  
+	    minute = minute < 10 ? ('0' + minute) : minute;    
+	    second = second < 10 ? ('0' + second) : second;   
+	    if(detail) return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;
+	    else return y + sign + m + sign + d;
+	}//end func
 	
 	//拷贝文本
 	function _copyText(e){
